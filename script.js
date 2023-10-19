9e0ab56fbeee5062fe231707aa493e1c4a24f794
kingster <57381795+kingster852@users.noreply.github.com>
2023-10-19 22:42:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ fetch('./data.json')
     // Process the JSON data
     console.log(data);
     console.log("check");
+    const red = document.getElementById("red");
+    red.innerHtml = "yoyoyo";
   })
   .catch(error => {
     // Handle any errors
     console.error('Error:', error);
   });
+  
